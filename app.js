b2be2bf41d982840fe382519170adc6b54161d41
arjit547 <97424724+arjit547@users.noreply.github.com>
2024-12-03 11:04:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('kubernetes-cicd-terraform');
+  res.end('kubernetes-CICD');
 });
 
 server.listen(3000, () => {
